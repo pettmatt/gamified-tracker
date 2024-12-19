eedb53d8d36738001a4cc1a941185b8a76e72257
pettmatt <petterimattila@protonmail.com>
2024-12-19 15:28:13 +0200

    Fixing woker implementation by adding Vite's own plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Checker from 'vite-plugin-checker'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import Checker from "vite-plugin-checker"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    Checker({ typescript: true }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        Checker({ typescript: true }),
+        VitePWA({
+            injectRegister: null,
+            registerType: "autoUpdate",
+            devOptions: {
+                enabled: true
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url))
+        }
     }
-  }
 })
